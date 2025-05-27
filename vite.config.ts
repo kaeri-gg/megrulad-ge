ca41d6ec254737a66963da830b7c76bf2f5ecf51
Kathleen Povadora <60345682+kaeri-gg@users.noreply.github.com>
2025-05-28 04:49:10 +0800

    updated packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  publicDir: 'public',
+  server: {
+    host: 'localhost',
+    port: 4096,
+    strictPort: true,
+    open: false,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:3001',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
