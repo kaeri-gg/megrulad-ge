6ee4c94c49c37273cee81120cf9bb503cb492491
Kathleen Povadora <60345682+kaeri-gg@users.noreply.github.com>
2026-01-16 16:22:35 +0400

    Add CommonWords component with styles
    Implemented the CommonWords component to display frequently searched words and added corresponding styles in common-words.css. Updated index.css to import the new stylesheet.

--- src/components/common-words/CommonWords.tsx
+++ src/components/common-words/CommonWords.tsx
@@ -1,5 +1,18 @@
 const CommonWords = () => {
-  return <div className="common-words-main-container"></div>;
+  return (
+    <div className="common-words-main-container">
+      <div className="common-words-header">
+        <h3>ხშირად მოძებნილი სიტყვები</h3>
+      </div>
+      <div className="common-words-container">
+        <span>საძიებო</span>
+        <span>სიტყვა</span>
+        <span>აუცილებლად</span>
+        <span>უნდა</span>
+        <span>შეიცავდეს</span>
+      </div>
+    </div>
+  );
 };
 
 export default CommonWords;
